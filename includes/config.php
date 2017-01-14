0acb26148c917c5449bdaaa23515915937ef4281
Sparshith Sampath <sparshithsampath@gmail.com>
2017-01-14 19:44:11 +0530

    minor fix

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('HOST', 'http://sparshith.com/');
+define('HOST', 'http://sparshith.com/korv3r/');
 define('APPLICATION_NAME', 'Google Calendar API PHP Quickstart');
 define('CLIENT_SECRET_PATH', __DIR__ . '/../client_secret.json');
 define('SCOPES', implode(' ', array(
